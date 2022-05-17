5d691c69aa44ddae0cb44a383da06034deaff9ce
dothatanditsdone <59626254+dothatanditsdone@users.noreply.github.com>
2022-05-17 13:14:07 -0400

    oh, i have to save the file first

--- file-reader/source_reader.py
+++ file-reader/source_reader.py
@@ -3,8 +3,8 @@
 import csv
 
 
-def watchFile (fileName):
-    with open(fileName, 'r') as curFile: #set file to variable
+def watchFile (fileLoc):
+    with open(fileLoc, 'r') as curFile: #set file to variable
         csv_reader = csv.reader(curFile) #read file to variable
         for csv_reader_line in csv_reader: #loop through csv
             print(csv_reader_line) #print to console for debugging
@@ -15,9 +15,9 @@ def watchFile (fileName):
                     read_list.append(item)
     return read_list
 
-#Set a file 
-fileName = 'E:\LocalRepository\Auto-Line-Printer\ALP_WatchFolder\SampleCSV.csv' #default file if nothing is passed to function
-read_list = watchFile(fileName)
+# Set a file to be the default in case no file location is passed to the function
+fileLoc = 'E:\LocalRepository\Auto-Line-Printer\ALP_WatchFolder\SampleCSV.csv' #default file if nothing is passed to function
+read_list = watchFile(fileLoc)
 
 
 print(read_list) #print the final array to console for debugging
