1b47fb6d30f419167f813458be666507afd000b2
dothatanditsdone <59626254+dothatanditsdone@users.noreply.github.com>
2022-05-16 21:15:30 -0400

    thanks sara!

--- file-reader/source_reader.py
+++ file-reader/source_reader.py
@@ -4,15 +4,25 @@ import csv
 
 #Default watch folder routine
 
-with open('E:\LocalRepository\Auto-Line-Printer\ALP_WatchFolder\SampleCSV.csv', 'r') as default_watchFile: #set default file to variable
-    csv_reader = csv.reader(default_watchFile) #read file to variable
-    for csv_reader_line in csv_reader: #loop through csv
-        print(csv_reader_line) #print to console for debugging
-        read_list = [] #empty array
-        for sublist in csv_reader: #loop through file, create array of elements
-            for item in sublist:
-                print(item)
-                read_list.append(item)
+
+def watchFile (fileName):
+    with open(fileName, 'r') as default_watchFile: #set default file to variable
+        csv_reader = csv.reader(default_watchFile) #read file to variable
+        for csv_reader_line in csv_reader: #loop through csv
+            print(csv_reader_line) #print to console for debugging
+            read_list = [] #empty array
+            for sublist in csv_reader: #loop through file, create array of elements
+                for item in sublist:
+                    print(item)
+                    read_list.append(item)
+    return read_list
+
+
+fileName = 'E:\LocalRepository\Auto-Line-Printer\ALP_WatchFolder\SampleCSV.csv'
+
+read_list = watchFile(fileName)
+
+
 print(read_list) #print the final array to console for debugging
 
 
